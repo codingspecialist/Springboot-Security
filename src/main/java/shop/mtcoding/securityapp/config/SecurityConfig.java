8357fd9e3ced79dfde7a8edade3db7f4ed312213
metacoding <getinthere@naver.com>
2023-04-17 01:22:46 +0900

    임포트 수정

--- src/main/java/shop/mtcoding/securityapp/config/SecurityConfig.java
+++ src/main/java/shop/mtcoding/securityapp/config/SecurityConfig.java
@@ -1,19 +1,10 @@
 package shop.mtcoding.securityapp.config;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 public class SecurityConfig {
